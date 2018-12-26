bf21a7804e92f6eadfd8617fd1621f6ba3d46c0b
QWQNI <1217852216@qq.com>
2018-12-26 15:11:39 +0800

    基础架构

---
+++ hotlemangement/hotlemangement/Controllers/staffsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using hotlemangement.Models;
+
+namespace hotlemangement.Controllers
+{
+    public class staffsController : Controller
+    {
+        private gdEntities db = new gdEntities();
+
+        // GET: staffs
+        public ActionResult Index()
+        {
+            return View(db.staff.ToList());
+        }
+
+        // GET: staffs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            staff staff = db.staff.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // GET: staffs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: staffs/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "name,num,compartment,ID")] staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.staff.Add(staff);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(staff);
+        }
+
+        // GET: staffs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            staff staff = db.staff.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: staffs/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "name,num,compartment,ID")] staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(staff).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(staff);
+        }
+
+        // GET: staffs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            staff staff = db.staff.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: staffs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            staff staff = db.staff.Find(id);
+            db.staff.Remove(staff);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ hotlemangement/hotlemangement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hotlemangement.MvcApplication" Language="C#" %>
